5826d813a7dcf83f59127972ade8ebe85c9b38cc
csergey8 <c.sergey@protonmail.com>
2018-01-02 20:20:49 +0200

    add delete functionality

--- public/app.js
+++ public/app.js
@@ -4,14 +4,50 @@ $(document).ready(function() {
     .catch((err) => {
       cosole.log(err);
     })
+
+  $('#todoInput').keypress((event) => {
+    if(event.which == 13) {
+      var val = $('#todoInput').val();
+      createTodo(val);
+    }
+  });
+
+  $('.list').on('click', 'span', function() {removeTodo($(this).parent()) });
 });
 
 function getTodos(todos) {
   todos.forEach((todo) => {
-    var newTodo = $('<li>' + todo.name + '</li>');
-    if(todo.completed) {
-      newTodo.addClass('done');
-    }
-    $('.list').append(newTodo);
+    addTodo(todo);
   })
 }
+
+function addTodo(todo) {
+  var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
+  newTodo.data('id', todo._id);
+  if(todo.completed) {
+    newTodo.addClass('done');
+  }
+  $('.list').append(newTodo);
+  $('#todoInput').val('');
+}
+
+function createTodo(todo) {
+  $.post('/api/todos', {name: todo})
+   .then((newTodo) => {
+     addTodo(newTodo);
+   })
+   .catch((err) => {
+     conosle.log(err);
+   })
+}
+
+function removeTodo(todo) {
+    var clickedId = todo.data('id');
+    $.ajax({
+      method: 'DELETE',
+      url: '/api/todos/' + clickedId
+    })
+      .then((data) => {
+        todo.remove();
+      })
+  }
