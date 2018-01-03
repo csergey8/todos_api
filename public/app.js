4abe7d1c5e81af4682b197e93d0db7de3abbb273
csergey8 <c.sergey@protonmail.com>
2018-01-03 09:49:10 +0200

    completed update

--- public/app.js
+++ public/app.js
@@ -12,7 +12,13 @@ $(document).ready(function() {
     }
   });
 
-  $('.list').on('click', 'span', function() {removeTodo($(this).parent()) });
+  $('.list').on('click', 'li', function() {
+    updateTodo($(this));
+  });
+
+  $('.list').on('click', 'span', function(e) {
+    e.stopPropagation();
+    removeTodo($(this).parent()) });
 });
 
 function getTodos(todos) {
@@ -24,6 +30,7 @@ function getTodos(todos) {
 function addTodo(todo) {
   var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
   newTodo.data('id', todo._id);
+  newTodo.data('completed', todo.completed);
   if(todo.completed) {
     newTodo.addClass('done');
   }
@@ -51,3 +58,20 @@ function removeTodo(todo) {
         todo.remove();
       })
   }
+function updateTodo(todo) {
+  var clickedId = todo.data('id');
+  var isDone = !todo.data('completed');
+  var updateData = {completed: isDone};
+  $.ajax({
+    method: 'PUT',
+    url: '/api/todos/' + clickedId,
+    data: updateData
+  })
+  .then(function(updatedTodo) {
+    todo.toggleClass("done");
+    todo.data('completed', isDone);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
